31bedb17e5df3e92b588964a0b51474000d23626
su20 <19liuq@elmbrookstudents.org>
2018-01-30 10:57:47 -0600

    all bill $1000 into the program

--- src/Intro/Atm.java
+++ src/Intro/Atm.java
@@ -1,18 +1,19 @@
 package Intro;
 
 public class Atm {
-private BillClass[] Level= {new BillClass(100, 10),new BillClass(50, 10),new BillClass(20, 10),new BillClass(10, 10),new BillClass(5, 10),new BillClass(1, 10)};
+private BillClass[] Level= {new BillClass(1000, 10), new BillClass(100, 10),new BillClass(50, 10),new BillClass(20, 10),new BillClass(10, 10),new BillClass(5, 10),new BillClass(1, 10)};
 private int totalValue;
 public Atm() {
 	totalValue=1860;
 }
 public void restock() {
-	Level[0]=new BillClass(100, 10);
-	Level[1]=new BillClass(50, 10);
-	Level[2]=new BillClass(20, 10);
-	Level[3]=new BillClass(10, 10);
-	Level[4]=new BillClass(5, 10);
-	Level[5]=new BillClass(1, 10);
+	Level[0]=new BillClass(1000,10);
+	Level[1]=new BillClass(100, 10);
+	Level[2]=new BillClass(50, 10);
+	Level[3]=new BillClass(20, 10);
+	Level[4]=new BillClass(10, 10);
+	Level[5]=new BillClass(5, 10);
+	Level[6]=new BillClass(1, 10);
 	totalValue=1860;
 }
 public void withDraw(int amount) {
@@ -36,28 +37,32 @@ private void recWithDraw(int amount, int currLevel) {
 }
 public void denomination(int chosenValue) {
 	switch(chosenValue) {
-	case 100:{
+	case 1000:{
 		System.out.println(Level[0].getQuantity());
 		break;
 	}
-	case 50:{
+	case 100:{
 		System.out.println(Level[1].getQuantity());
 		break;
 	}
-	case 20:{
+	case 50:{
 		System.out.println(Level[2].getQuantity());
 		break;
 	}
-	case 10:{
+	case 20:{
 		System.out.println(Level[3].getQuantity());
 		break;
 	}
-	case 5:{
+	case 10:{
 		System.out.println(Level[4].getQuantity());
 		break;
 	}
-	case 1:{
+	case 5:{
 		System.out.println(Level[5].getQuantity());
+		break;
+	}
+	case 1:{
+		System.out.println(Level[6].getQuantity());
 		break;
 	}
 	}
